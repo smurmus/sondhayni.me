f10a26f12220b7a25e02c29e79048ac0c01a3a34
Sondhayni <sondhayni@gmail.com>
2021-01-22 19:57:35 -0800

    add resume link to nav

--- src/components/nav/NavLinks.jsx
+++ src/components/nav/NavLinks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import { red, darkBlue } from '../../utils/colors';
 
@@ -22,13 +22,51 @@ const NavLink = styled(Link)`
   }
 `;
 
-const NavLinks = () => (
-  <>
-    <NavLink to="/about">about</NavLink>
-    <NavLink to="/#projects">work</NavLink>
-    <NavLink to="/sandbox">sandbox</NavLink>
-    {/* <NavLink to="/blog">writings</NavLink> */}
-  </>
-);
+const ResumeLink = styled.a`
+  text-decoration: none;
+  font-family: Barlow;
+  font-size: 18px;
+  color: ${darkBlue};
+  margin: 0 16px;
+  font-weight: 600;
 
+  &:hover {
+    color: ${red};
+  }
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+    margin-bottom: 32px;
+  }
+`;
+
+const NavLinks = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allFile(filter: {name: {eq: "Sondhayni_Murmu_Design_Resume" }}) {
+      edges {
+        node {
+          id
+          publicURL
+        }
+      }
+    }
+  }
+`);
+
+  const resumeURL = data.allFile.edges[0].node.publicURL;
+
+  return (
+    <>
+      <NavLink to="/about">about</NavLink>
+      <NavLink to="/#projects">work</NavLink>
+      <NavLink to="/sandbox">sandbox</NavLink>
+      <ResumeLink href={resumeURL} download>
+        resume
+        {/* {file.node.name} */}
+      </ResumeLink>
+      {/* <NavLink to="/blog">writings</NavLink> */}
+    </>
+  );
+};
 export default NavLinks;
